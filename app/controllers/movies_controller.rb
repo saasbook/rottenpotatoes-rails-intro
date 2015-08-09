2d813348035b8d87eb4a30e0b73d8dd1b67abcbe
Armando Fox <fox@cs.berkeley.edu>
2015-08-08 19:24:10 -0700

    adding base files from rottenpotatoes app

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,45 @@
+class MoviesController < ApplicationController
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
+    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
+      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
+      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
+      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
+      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+  	 ]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
