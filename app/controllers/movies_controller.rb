22947e826fbbb504573ca7b7164f3dbfba9136ab
Nathan Smalley <nathan@smalley.io>
2020-02-28 02:03:31 +0000

    complete part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,21 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort_by] == 'title'
+      @movies = Movie.order(:title)
+      @movie_title_th_classes = 'hilite'
+      
+    elsif params[:sort_by] == 'release_date'
+      @movies = Movie.order(:release_date)
+      @movie_release_date_th_classes = 'hilite'
+      
+    else
+      @movies = Movie.all
+      
+    end
+    
+    
+    
   end
 
   def new
