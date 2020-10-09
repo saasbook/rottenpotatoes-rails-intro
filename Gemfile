1a1f7b18285c91f55d7695ff3f10a96083a71b05
Armando Fox <fox@cs.berkeley.edu>
2020-10-08 17:22:30 -0700

    instructions removed since this repo is not a standalone assignment

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '~> 2.6.4'
+ruby '>= 2.6.4', '< 3.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.11.3'
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 
-def show
+  def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
